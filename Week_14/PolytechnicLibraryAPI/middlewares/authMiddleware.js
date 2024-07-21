b59ddc2913a521c1da57785b1c9e0a193e236470
awzhenkang <s10241668@connect.np.edu.sg>
2024-07-21 16:02:55 +0800

    minor changes to wk 14 practical

--- Week_14/PolytechnicLibraryAPI/middlewares/authMiddleware.js
+++ Week_14/PolytechnicLibraryAPI/middlewares/authMiddleware.js
@@ -1,43 +1,45 @@
 const jwt = require("jsonwebtoken");
-const Joi = require("joi");
+
+const secretKey = process.env.JWT_SECRET_KEY; 
 
 function verifyJWT(req, res, next) {
-    const token = req.headers.authorization && req.headers.authorization.split(" ")[1];
-  
-    if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
-  
-    jwt.verify(token, "your_secret_key", (err, decoded) => {
-      if (err) {
-        return res.status(403).json({ message: "Forbidden" });
-      }
-  
-      // Define authorized roles for each endpoint
-      const authorizedRoles = {
-        "^/books$": ["member", "librarian"], // Anyone can view books
-        
-        "^/books/[0-9]+/availability$": ["librarian"], // Only librarians can update availability
-      };
-  
-      const requestedEndpoint = req.url;
-      const userRole = decoded.role;
-  
-      // Check if the user's role is authorized for the requested endpoint
-      const authorizedRole = Object.entries(authorizedRoles).find(
-        ([endpoint, roles]) => {
-          const regex = new RegExp(endpoint); // Create RegExp from endpoint
-          return regex.test(requestedEndpoint) && roles.includes(userRole);
-        }
-      );
-  
-      if (!authorizedRole) {
-        return res.status(403).json({ message: "Forbidden" });
-      }
-  
-      req.user = decoded; 
-      next();
-    });
+  const token = req.headers.authorization && req.headers.authorization.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
-  
-  module.exports = verifyJWT;
+
+  // verify JWT token
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+
+    
+    const authorizedRoles = {
+      "^/books$": ["member", "librarian"], // members and librarians can view books
+      "^/books/[0-9]+/availability$": ["librarian"], // only librarians can update availability
+
+    };
+
+    const requestedEndpoint = req.path; 
+    const userRole = decoded.role;
+
+    
+    const authorizedRole = Object.entries(authorizedRoles).find(
+      ([endpoint, roles]) => {
+        const regex = new RegExp(endpoint);
+        return regex.test(requestedEndpoint) && roles.includes(userRole);
+      }
+    );
+
+    if (!authorizedRole) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+
+    req.user = decoded; 
+    next();
+  });
+}
+
+module.exports = verifyJWT;
