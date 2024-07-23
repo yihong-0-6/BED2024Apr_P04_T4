19cf7367e9ae2a1b3960b2d14ab10183e05fdb0d
awzhenkang <s10241668@connect.np.edu.sg>
2024-07-23 09:36:48 +0800

    forum changes

---
+++ BED/controllers/forumController.js
@@ -0,0 +1,72 @@
+const Forum = require("../models/forum");
+
+// Create new forum
+const createForum = async (req, res) => {
+  const { title, author, comments } = req.body;
+  try {
+    const result = await Post.createPost(title, author, comments);
+
+    console.log('New forum created successfully:', result);
+
+    res.redirect('/Community');
+  } 
+
+  catch (err) {
+    console.error('Error creating forum:', err);
+    res.status(500).send('Error creating forum');
+  }
+};
+
+const getForumById = async (req, res) => {
+  const forumId = parseInt(req.params.forumId);
+  try {
+    const forum = await Forum.getForumById(forumId);
+    if (!forum) {
+      return res.status(404).send("Forum not found");
+    }
+    res.json(forum);
+  } 
+  
+  catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving forum");
+  }
+};
+
+// Get all forums
+const getAllForums = async (req, res) => {
+  try {
+    const forums = await Forum.getAllForums();
+    res.json(forums);
+  } 
+  catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving forums");
+  }
+};
+
+// Remove a forum
+const deleteForum = async (req, res) => {
+  const forumId = parseInt(req.params.forumId);
+
+  try {
+    const success = await Forum.deletePost(forumId);
+
+    if (!success) {
+      return res.status(404).send("Forum not found");
+    }
+    res.status(204).send();
+  } 
+  
+  catch (error) {
+    console.error(error);
+    res.status(500).send("Error deleting forum");
+  }
+};
+
+module.exports = {
+  createForum,
+  getForumById,
+  getAllForums,
+  deleteForum
+};
---
+++ BED/middlewares/validateMovie.js
@@ -0,0 +1,20 @@
+const Joi = require("joi");
+
+const validateMovie = (req, res, next) => {
+    const schema = Joi.object({
+        Name: Joi.string().min(3).max(100).required(),
+        Director: Joi.string().min(3).max(100).required(),
+    });
+
+    const validation = schema.validate(req.body, { abortEarly: false }); // Validate request body
+    
+    if (validation.error) {
+        const errors = validation.error.details.map((error) => error.message);
+        res.status(400).json({ message: "Validation error", errors });
+        return; // Terminate middleware execution on validation error
+    }
+
+    next(); // If validation passes, proceed to the next route handler
+};
+
+module.exports = validateMovie
